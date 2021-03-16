f44f911978b778bba6dc4b6fd40b9e40ad69e2c3
Edoardo Tenani <edoardo.tenani@protonmail.com>
2021-03-16 23:06:09 +0100

    archive: implement encrypted archive

---
+++ internal/archive/encrypted/encrypted.go
@@ -0,0 +1,30 @@
+package encrypted
+
+import (
+	"fmt"
+	"io"
+
+	"filippo.io/age"
+	compressedarchive "github.com/endorama/devid/internal/archive/compressed"
+)
+
+// Create creates an encrypted compressed tar archive
+func Create(out io.Writer, files []string, password string) error {
+	r, err := age.NewScryptRecipient(password)
+	if err != nil {
+		return fmt.Errorf("failed creating scrypt recipient: %w", err)
+	}
+
+	w, err := age.Encrypt(out, r)
+	if err != nil {
+		return fmt.Errorf("failed encrypting: %w", err)
+	}
+	defer w.Close()
+
+	err = compressedarchive.Create(w, files)
+	if err != nil {
+		return fmt.Errorf("cannot create archive: %w", err)
+	}
+
+	return nil
+}
