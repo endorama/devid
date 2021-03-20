70bd4f10427a2a3fd96b709cb1beb9d4175b8430
Edoardo Tenani <edoardo.tenani@protonmail.com>
2021-03-20 01:06:19 +0100

    use utils.WalkRelative

--- internal/backup/task.go
+++ internal/backup/task.go
@@ -1,11 +1,9 @@
 package backup
 
 import (
-	"fmt"
 	"io"
-	"os"
-	"path/filepath"
-	"strings"
+
+	"github.com/endorama/devid/internal/utils"
 )
 
 // Task contains required information for backup operation.
@@ -20,39 +18,5 @@ type Task struct {
 
 // Files retrieves list of files to backup by walking Source.
 func (b Task) Files() ([]string, error) {
-	files := []string{}
-
-	info, err := os.Stat(b.Source)
-	if os.IsNotExist(err) {
-		return files, fmt.Errorf("folder does not exists: %w", err)
-	}
-
-	if !info.IsDir() {
-		return files, fmt.Errorf("source is not a folder")
-	}
-
-	walkErrs := []error{}
-	err = filepath.Walk(b.Source,
-		func(path string, info os.FileInfo, err error) error {
-			if err != nil {
-				walkErrs = append(walkErrs, err)
-			}
-			if !info.IsDir() {
-				// strip prefix so file path is relative to source root
-				files = append(files,
-					strings.ReplaceAll(path, fmt.Sprintf("%s/", b.Source), ""))
-			}
-
-			return nil
-		})
-
-	if err != nil {
-		return files, fmt.Errorf("backup source walk failed: %w", err)
-	}
-
-	if len(walkErrs) != 0 {
-		return files, fmt.Errorf("errors walking source: %s", walkErrs)
-	}
-
-	return files, nil
+	return utils.WalkRelative(b.Source)
 }
