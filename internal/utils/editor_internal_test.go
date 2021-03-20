e3f0cdd13a0c49e0218af8044c7599d3307493a0
Edoardo Tenani <edoardo.tenani@protonmail.com>
2021-03-20 01:09:44 +0100

    add utils editor internal tests

---
+++ internal/utils/editor_internal_test.go
@@ -0,0 +1,30 @@
+package utils //nolint:testpackage // tests for internal API
+
+import "testing"
+
+func Test_isEditorAllowed(t *testing.T) {
+	type args struct {
+		editorCmd string
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"ed (fullpath)", args{"/bin/ed"}, true},
+		{"nano (fullpath)", args{"/bin/nano"}, true},
+		{"vim (fullpath)", args{"/usr/bin/vim"}, true},
+		{"vim", args{"vim"}, true},
+		{"nano", args{"nano"}, true},
+		{"not allowed", args{"/something/else"}, false},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isEditorAllowed(tt.args.editorCmd); got != tt.want {
+				t.Errorf("isEditorAllowed() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
