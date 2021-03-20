4ce120969fcc2c2f2dc022451ae3d6ac3442eb7c
Edoardo Tenani <edoardo.tenani@protonmail.com>
2021-03-20 01:06:41 +0100

    add archive.Create test

---
+++ internal/archive/archive_test.go
@@ -0,0 +1,30 @@
+package archive_test
+
+import (
+	"archive/tar"
+	"bytes"
+	"testing"
+
+	"github.com/endorama/devid/internal/archive"
+	"github.com/endorama/devid/internal/utils"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestArchive_Create(t *testing.T) {
+	files, err := utils.Walk("testdata")
+	if err != nil {
+		panic(err)
+	}
+
+	assert.ElementsMatch(t, files, []string{"testdata/file.yaml"}, "do not match")
+
+	var out bytes.Buffer
+	err = archive.Create(&out, files)
+	assert.NoError(t, err, "no error expected here")
+
+	// test reading from the resulting buffer
+	// this test does not check the content, only that a valid tar has been created
+	tarReader := tar.NewReader(&out)
+	_, err = tarReader.Next()
+	assert.NoError(t, err, "reading should work")
+}
