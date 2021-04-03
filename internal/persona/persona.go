152975391231ed589bf6cd180820f02f5594c6d2
Edoardo Tenani <edoardo.tenani@protonmail.com>
2021-04-04 00:37:59 +0200

    persona: load plugins and configurations

--- internal/persona/persona.go
+++ internal/persona/persona.go
@@ -2,10 +2,14 @@ package persona
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"path"
 
 	"github.com/endorama/devid/internal/plugin"
 	"github.com/endorama/devid/internal/utils"
+	"github.com/endorama/devid/plugins/identity"
+	"gopkg.in/yaml.v1"
 )
 
 const (
@@ -54,7 +58,30 @@ func (p Persona) Name() string {
 	return p.name
 }
 
-func (p Persona) Load() error {
+func (p *Persona) Load() error {
+	yamlFile, err := ioutil.ReadFile(p.File())
+	if err != nil {
+		return fmt.Errorf("cannot read yaml file: %w", err)
+	}
+
+	var fromConfig struct {
+		APIVersion string   `yaml:"apiVersion"`
+		Plugins    []string `yaml:"plugins"`
+	}
+	err = yaml.Unmarshal(yamlFile, &fromConfig)
+	if err != nil {
+		return fmt.Errorf("cannot unmarshal yaml file: %w", err)
+	}
+
+	if fromConfig.APIVersion != apiVersion {
+		return fmt.Errorf("unsupported APIVersion")
+	}
+
+	p.APIVersion = fromConfig.APIVersion
+
+	p.loadPlugins(fromConfig.Plugins)
+	p.loadPluginsConfiguration(yamlFile)
+
 	return nil
 }
 
@@ -63,7 +90,16 @@ func (p Persona) Whoami() string {
 	return p.name
 }
 
-func (p Persona) loadPlugins() {}
+func (p Persona) loadPlugins(plugins []string) {
+	for _, pluginName := range plugins {
+		switch pluginName {
+		case identity.PluginName:
+			p.enablePlugin(identity.NewPlugin())
+		default:
+			panic(fmt.Sprintf("trying to load unknown plugin: %s", pluginName))
+		}
+	}
+}
 
 func (p *Persona) enablePlugin(pg plugin.Pluggable) {
 	if p.Plugins[pg.Name()] == nil {
@@ -71,8 +107,28 @@ func (p *Persona) enablePlugin(pg plugin.Pluggable) {
 	}
 }
 
-func (p *Persona) disablePlugin(pg plugin.Pluggable) {
-	if p.Plugins[pg.Name()] != nil {
-		p.Plugins[pg.Name()] = nil
+//
+// func (p *Persona) disablePlugin(pg plugin.Pluggable) {
+//   if p.Plugins[pg.Name()] != nil {
+//     p.Plugins[pg.Name()] = nil
+//   }
+// }
+
+func (p *Persona) loadPluginsConfiguration(configFile []byte) error {
+	var errs []error
+
+	for _, plg := range p.Plugins {
+		if configurablePlugin, ok := plg.(plugin.Configurable); ok {
+			err := configurablePlugin.LoadConfig(configFile)
+			if err != nil {
+				errs = append(errs, err)
+			}
+			log.Printf(fmt.Sprintf("  %+v\n", configurablePlugin.Config()))
+		}
 	}
+	if len(errs) > 0 {
+		return fmt.Errorf("error loading plugin configuration: %s", errs)
+	}
+
+	return nil
 }
