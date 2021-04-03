e46d6d56d9276281fd3a0cec1f8179f0de3d6413
Edoardo Tenani <edoardo.tenani@protonmail.com>
2021-04-04 00:32:01 +0200

    plugin: extract Renderable interface

--- internal/plugin/plugin.go
+++ internal/plugin/plugin.go
@@ -4,14 +4,8 @@ package plugin
 // Is the common interface implemented by all plugins.
 type Pluggable interface {
 	Name() string
-	Renderable
 }
 
-// Renderable interface allow a plugin to render content in the shell loader
-// and runner files.
-type Renderable interface {
-	Render(name, location string) string
-}
 
 // Setuppable interface allow a plugin to perform setup steps before rendering.
 type Setuppable interface {
@@ -29,4 +23,10 @@ type Configurable interface {
 // Generator interface allow a plugin to generate content before rendering.
 type Generator interface {
 	Generate(location string) error
+}
+
+// Renderable interface allow a plugin to render content in the shell loader
+// and runner files.
+type Renderable interface {
+	Render(name, location string) string
 }
