14686fde626f1c2f8ada132a97172e3f52659d4e
Edoardo Tenani <edoardo.tenani@protonmail.com>
2021-04-04 00:49:04 +0200

    persona,loader: extract plugin loading to dedicated package

---
+++ internal/loader/loader.go
@@ -0,0 +1,70 @@
+package loader
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	"github.com/endorama/devid/internal/persona"
+	"github.com/endorama/devid/internal/plugin"
+	"github.com/endorama/devid/plugins/identity"
+	"gopkg.in/yaml.v1"
+)
+
+func LoadPlugins(p *persona.Persona) error {
+	yamlFile, err := ioutil.ReadFile(p.File())
+	if err != nil {
+		return fmt.Errorf("cannot read yaml file: %w", err)
+	}
+
+	var fromConfig struct {
+		APIVersion string   `yaml:"apiVersion"`
+		Plugins    []string `yaml:"plugins"`
+	}
+	err = yaml.Unmarshal(yamlFile, &fromConfig)
+	if err != nil {
+		return fmt.Errorf("cannot unmarshal yaml file: %w", err)
+	}
+
+	if fromConfig.APIVersion != p.APIVersion {
+		return fmt.Errorf("unsupported APIVersion")
+	}
+
+	p.APIVersion = fromConfig.APIVersion
+
+	loadPlugins(*p, fromConfig.Plugins)
+	loadPluginsConfiguration(*p, yamlFile)
+
+	return nil
+
+}
+
+func loadPlugins(p persona.Persona, plugins []string) {
+	for _, pluginName := range plugins {
+		switch pluginName {
+		case identity.PluginName:
+			p.EnablePlugin(identity.NewPlugin())
+		default:
+			panic(fmt.Sprintf("trying to load unknown plugin: %s", pluginName))
+		}
+	}
+}
+
+func loadPluginsConfiguration(p persona.Persona, configFile []byte) error {
+	var errs []error
+
+	for _, plg := range p.Plugins {
+		if configurablePlugin, ok := plg.(plugin.Configurable); ok {
+			err := configurablePlugin.LoadConfig(configFile)
+			if err != nil {
+				errs = append(errs, err)
+			}
+			log.Printf(fmt.Sprintf("  %+v\n", configurablePlugin.Config()))
+		}
+	}
+	if len(errs) > 0 {
+		return fmt.Errorf("error loading plugin configuration: %s", errs)
+	}
+
+	return nil
+}
--- internal/persona/persona.go
+++ internal/persona/persona.go
@@ -1,15 +1,10 @@
 package persona
 
 import (
-	"fmt"
-	"io/ioutil"
-	"log"
 	"path"
 
 	"github.com/endorama/devid/internal/plugin"
 	"github.com/endorama/devid/internal/utils"
-	"github.com/endorama/devid/plugins/identity"
-	"gopkg.in/yaml.v1"
 )
 
 const (
@@ -58,77 +53,21 @@ func (p Persona) Name() string {
 	return p.name
 }
 
-func (p *Persona) Load() error {
-	yamlFile, err := ioutil.ReadFile(p.File())
-	if err != nil {
-		return fmt.Errorf("cannot read yaml file: %w", err)
-	}
-
-	var fromConfig struct {
-		APIVersion string   `yaml:"apiVersion"`
-		Plugins    []string `yaml:"plugins"`
-	}
-	err = yaml.Unmarshal(yamlFile, &fromConfig)
-	if err != nil {
-		return fmt.Errorf("cannot unmarshal yaml file: %w", err)
-	}
-
-	if fromConfig.APIVersion != apiVersion {
-		return fmt.Errorf("unsupported APIVersion")
-	}
-
-	p.APIVersion = fromConfig.APIVersion
-
-	p.loadPlugins(fromConfig.Plugins)
-	p.loadPluginsConfiguration(yamlFile)
-
-	return nil
-}
-
 // Whoami returns human readable identity information.
 func (p Persona) Whoami() string {
 	return p.name
 }
 
-func (p Persona) loadPlugins(plugins []string) {
-	for _, pluginName := range plugins {
-		switch pluginName {
-		case identity.PluginName:
-			p.enablePlugin(identity.NewPlugin())
-		default:
-			panic(fmt.Sprintf("trying to load unknown plugin: %s", pluginName))
-		}
-	}
-}
-
-func (p *Persona) enablePlugin(pg plugin.Pluggable) {
+// EnablePlugin adds an initialized plugin from the Plugins list.
+func (p *Persona) EnablePlugin(pg plugin.Pluggable) {
 	if p.Plugins[pg.Name()] == nil {
 		p.Plugins[pg.Name()] = pg
 	}
 }
 
-//
-// func (p *Persona) disablePlugin(pg plugin.Pluggable) {
-//   if p.Plugins[pg.Name()] != nil {
-//     p.Plugins[pg.Name()] = nil
-//   }
-// }
-
-func (p *Persona) loadPluginsConfiguration(configFile []byte) error {
-	var errs []error
-
-	for _, plg := range p.Plugins {
-		if configurablePlugin, ok := plg.(plugin.Configurable); ok {
-			err := configurablePlugin.LoadConfig(configFile)
-			if err != nil {
-				errs = append(errs, err)
-			}
-			log.Printf(fmt.Sprintf("  %+v\n", configurablePlugin.Config()))
-		}
+// DisablePlugin removes a plugin from the Plugins list.
+func (p *Persona) DisablePlugin(pg plugin.Pluggable) {
+	if p.Plugins[pg.Name()] != nil {
+		p.Plugins[pg.Name()] = nil
 	}
-	if len(errs) > 0 {
-		return fmt.Errorf("error loading plugin configuration: %s", errs)
-	}
-
-	return nil
 }
