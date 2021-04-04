cfba88bbf0da464cddbde702b0548a9bffd8bef4
Edoardo Tenani <edoardo.tenani@protonmail.com>
2021-04-04 02:17:34 +0200

    fix wrapcheck

--- internal/utils/file.go
+++ internal/utils/file.go
@@ -1,18 +1,21 @@
 package utils
 
-import "os"
+import (
+	"fmt"
+	"os"
+)
 
 // PersistFile writes content to a path.
 func PersistFile(path, content string) error {
 	file, err := os.Create(path)
 	if err != nil {
-		return err
+		return fmt.Errorf("cannot craete path: %w", err)
 	}
 	defer file.Close()
 
 	_, err = file.WriteString(content)
 	if err != nil {
-		return err
+		return fmt.Errorf("cannot write file: %w", err)
 	}
 
 	return nil
@@ -27,7 +30,7 @@ func PersistExecutableFile(path, content string) error {
 
 	err = os.Chmod(path, 0700)
 	if err != nil {
-		return err
+		return fmt.Errorf("cannot change file permissions: %w", err)
 	}
 
 	return nil
