e7486cbdb0045a76789aaca0ef3fa03b058d6028
Edoardo Tenani <edoardo.tenani@pm.me>
2021-07-03 16:00:13 +0200

    update plugin template

---
+++ plugins/_template/pluggable.go
@@ -0,0 +1,7 @@
+package name
+
+// Name return plugin name.
+// Implements `plugin.Pluggable` interface.
+func (p Plugin) Name() string {
+	return pluginName
+}
--- plugins/_template/plugin.go
+++ plugins/_template/plugin.go
@@ -10,9 +10,3 @@ type Plugin struct {
 func NewPlugin() *Plugin {
 	return &Plugin{}
 }
-
-// Name return plugin name.
-// Implements `plugin.Pluggable` interface.
-func (p Plugin) Name() string {
-	return pluginName
-}
