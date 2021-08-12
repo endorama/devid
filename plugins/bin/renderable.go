a7be795674a2d333d4a33800b5e1d899cb47eb36
Edoardo Tenani <edoardo.tenani@pm.me>
2021-08-13 01:27:36 +0200

    bin: double quote export

--- plugins/bin/renderable.go
+++ plugins/bin/renderable.go
@@ -4,7 +4,7 @@ import "strings"
 
 func (p Plugin) Render(profileName, profileLocation string) string {
 	sb := strings.Builder{}
-	sb.WriteString("export PATH=" + profileLocation + "/bin:$PATH\n")
+	sb.WriteString("export PATH=\"" + profileLocation + "/bin:$PATH\"\n")
 
 	return sb.String()
 }
