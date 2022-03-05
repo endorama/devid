aab9dd4e664a7a72143d010c4d0db88f6eff1ee0
Edoardo Tenani <edoardo.tenani@pm.me>
2022-03-05 18:14:04 +0100

    ssh: create known_hosts and config

--- plugins/ssh/setuppable.go
+++ plugins/ssh/setuppable.go
@@ -1,6 +1,7 @@
 package ssh
 
 import (
+	"fmt"
 	"os"
 	"path"
 )
@@ -10,5 +11,22 @@ const sshFolderPerm = os.FileMode(0750)
 func (p Plugin) Setup(profileLocation string) error {
 	loc := path.Join(profileLocation, pluginName)
 
-	return p.fs.MkdirAll(loc, sshFolderPerm)
+	err := p.fs.MkdirAll(loc, sshFolderPerm)
+	if err != nil {
+		return fmt.Errorf("cannot create ssh plugin folder: %w", err)
+	}
+
+	knownHostsFile, err := os.Create(path.Join(loc, "known_hosts"))
+	if err != nil {
+		return fmt.Errorf("cannot create known_hosts file: %w", err)
+	}
+	knownHostsFile.Close()
+
+	configFile, err := os.Create(path.Join(loc, "config"))
+	if err != nil {
+		return fmt.Errorf("cannot create config file: %w", err)
+	}
+	configFile.Close()
+
+	return nil
 }
