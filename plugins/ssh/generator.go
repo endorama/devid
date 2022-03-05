5979045f95f03752e9f974583c40e787f8fd8211
Edoardo Tenani <edoardo.tenani@pm.me>
2022-03-05 18:14:29 +0100

    ssh: create ssh and scp binstubs

--- plugins/ssh/generator.go
+++ plugins/ssh/generator.go
@@ -1,15 +1,43 @@
 package ssh
 
-import "github.com/endorama/devid/internal/plugin"
+import (
+	"fmt"
+	"path"
+	"strings"
+
+	"github.com/endorama/devid/internal/plugin"
+)
 
 // Generate a file in the bin plugin directory.
 // Implements the Generator interface.
 func (p *Plugin) Generate(personaDirectory string) (plugin.Generated, error) {
-	// TODO: implement file generation
-	// file := strings.Builder{}
+	sshPluginFolder := path.Join(personaDirectory, pluginName)
+
+	knownHostsFile := path.Join(sshPluginFolder, "known_hosts")
+	knownHostsOption := fmt.Sprintf("-o UserKnownHostsFile=%s ", knownHostsFile)
+
+	configFile := path.Join(sshPluginFolder, "config")
+	configOption := fmt.Sprintf("-F %s ", configFile)
+
+	ssh := strings.Builder{}
+	ssh.WriteString("#!/usr/bin/env bash\n")
+	ssh.WriteString("exec /usr/bin/ssh ")
+	ssh.WriteString(knownHostsOption)
+	ssh.WriteString(configOption)
+	ssh.WriteString("$@")
+
+	scp := strings.Builder{}
+	scp.WriteString("#!/usr/bin/env bash\n")
+	scp.WriteString("exec /usr/bin/scp ")
+	scp.WriteString(knownHostsOption)
+	scp.WriteString(configOption)
+	scp.WriteString("$@")
 
 	return plugin.Generated{
-		Executables: []plugin.GeneratedFile{},
-		Files:       []plugin.GeneratedFile{},
+		Executables: []plugin.GeneratedFile{
+			{Name: "ssh", Content: ssh.String()},
+			{Name: "scp", Content: scp.String()},
+		},
+		Files: []plugin.GeneratedFile{},
 	}, nil
 }
