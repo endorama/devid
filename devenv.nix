36053039fd47d31ebc8c9aa9f2f63f3170ae6ac6
Edoardo Tenani <526307+endorama@users.noreply.github.com>
2023-06-08 18:50:16 +0200

    add devenv.sh (#4)

---
+++ devenv.nix
@@ -0,0 +1,33 @@
+{ pkgs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = [
+    pkgs.git
+    pkgs.go
+    pkgs.go-task
+  ];
+
+  # https://devenv.sh/scripts/
+  scripts.hello.exec = "echo hello from $GREET";
+
+  enterShell = ''
+    git --version
+    go version
+    task --version
+  '';
+
+  # https://devenv.sh/languages/
+  # languages.nix.enable = true;
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.ping.exec = "ping example.com";
+
+  # See full reference at https://devenv.sh/reference/options/
+}
