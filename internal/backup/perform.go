3238a3d2f87bfa94e096898f485163014136152a
Edoardo Tenani <edoardo.tenani@pm.me>
2024-06-24 01:49:12 +0200

    restore working dir after backup
    backup.Perform was using os.Chdir, which changes working
    directory for the entire process. This in practice did not
    have any effect, but is the potential source of future bugs
    as it's an unexpected occurrence.
    
    Solve this by restoring working directory after executing
    the backup.
    A dedicated test case is provided.

--- internal/backup/perform.go
+++ internal/backup/perform.go
@@ -8,26 +8,51 @@ import (
 )
 
 // Perform create an encrypted backup archive from the specified set of files, using the
-// specified passphrase. To support relative paths, it allows a cwd parameter to change directory
-// before creating the file.
+// specified passphrase. To support relative paths, it changes cwd before creating the file.
 // Archive will be created in the current folder.
-func Perform(b Task, passphrase string) error {
+// TODO: consider removing and use Task.Perform()
+func Perform(b Task, passphrase string) (err error) {
 	// NOTE: change folder to source location, as b.Files() return relative
 	// paths for files to be added to the archive
-	err := os.Chdir(b.Source)
+	return wrapChdirRestore(b.Source, func() error {
+		files, err := b.Files()
+		if err != nil {
+			return fmt.Errorf("failed retrieving files to backup: %w", err)
+		}
+
+		err = encryptedarchive.Create(b.Destination, files, passphrase)
+		if err != nil {
+			return fmt.Errorf("failed creating encrypted backup archive: %w", err)
+		}
+
+		return nil
+	})
+}
+
+// wrapChdirRestore change folder to dir, execute the specified function
+// and restore the working directory as set before the change.
+func wrapChdirRestore(target string, f func() error) (err error) {
+	var cwd string
+
+	cwd, err = os.Getwd()
+	if err != nil {
+		return fmt.Errorf("cannot get current working directory: %w", err)
+	}
+
+	defer func() {
+		// NOTE: In case changing back to the previous working directory
+		// fails is possible to shadow the error of the passed in function.
+		// This is acceptable as it is not considered common to not be able
+		// to chdir to a previously working directory.
+		err = os.Chdir(cwd)
+	}()
+
+	err = os.Chdir(target)
 	if err != nil {
 		return fmt.Errorf("cannot change dir: %w", err)
 	}
 
-	files, err := b.Files()
-	if err != nil {
-		return fmt.Errorf("failed retrieving files to backup: %w", err)
-	}
+	err = f()
 
-	err = encryptedarchive.Create(b.Destination, files, passphrase)
-	if err != nil {
-		return fmt.Errorf("failed creating encrypted backup archive: %w", err)
-	}
-
-	return nil
+	return err
 }
---
+++ internal/backup/perform_test.go
@@ -0,0 +1,49 @@
+package backup_test
+
+import (
+	"bytes"
+	"os"
+	"testing"
+
+	"github.com/endorama/devid/internal/backup"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPerform(t *testing.T) {
+	t.Skip("not implemented")
+	type args struct {
+		b          backup.Task
+		passphrase string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := backup.Perform(tt.args.b, tt.args.passphrase); (err != nil) != tt.wantErr {
+				t.Errorf("Perform() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestPerform_Chdir(t *testing.T) {
+	var out bytes.Buffer
+	tk, err := backup.NewTask("test", "testdata", &out)
+	assert.NoError(t, err)
+
+	want, err := os.Getwd()
+	assert.NoError(t, err)
+
+	err = backup.Perform(tk, "test")
+	assert.NoError(t, err)
+
+	got, err := os.Getwd()
+	assert.NoError(t, err)
+
+	assert.Equal(t, want, got)
+}
